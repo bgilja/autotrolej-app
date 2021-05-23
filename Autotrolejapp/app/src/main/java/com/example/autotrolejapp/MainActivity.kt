da9ea1a80dd458d3a9f864684e223b70b46f63e9
bgilja <borna.gilja@gmail.com>
2021-05-23 19:16:28 +0200

    Created new project, added bottom nav (meow)

---
+++ Autotrolejapp/app/src/main/java/com/example/autotrolejapp/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.autotrolejapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val ID_HOME = HomeFragment.FRAGMENT_ID
+        private const val ID_MAP = MapFragment.FRAGMENT_ID
+        private const val ID_BUS = BusFragment.FRAGMENT_ID
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val fragment = supportFragmentManager.fragments.last()
+
+        val bottomNavigation = findViewById<MeowBottomNavigation>(R.id.bottomNavigation)
+        if (fragment is HomeFragment) {
+            bottomNavigation.show(ID_HOME)
+        } else if (fragment is MapFragment) {
+            bottomNavigation.show(ID_MAP)
+        } else if (fragment is BusFragment) {
+            bottomNavigation.show(ID_BUS)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<MeowBottomNavigation>(R.id.bottomNavigation)
+        bottomNavigation.add(MeowBottomNavigation.Model(ID_HOME, R.drawable.ic_baseline_home_24))
+        bottomNavigation.add(MeowBottomNavigation.Model(ID_MAP, R.drawable.ic_baseline_map_24))
+        bottomNavigation.add(MeowBottomNavigation.Model(ID_BUS, R.drawable.ic_baseline_directions_bus_24))
+
+        bottomNavigation.show(ID_HOME)
+        replaceFragment(HomeFragment.newInstance())
+
+        bottomNavigation.setOnClickMenuListener {
+            activateFragment(it.id)
+        }
+    }
+
+    private fun activateFragment(fragmentId: Int) {
+        when(fragmentId) {
+            ID_HOME -> {
+                Toast.makeText(this@MainActivity, "Home fragment", Toast.LENGTH_SHORT).show()
+                replaceFragment(HomeFragment.newInstance())
+            }
+            ID_MAP -> {
+                Toast.makeText(this@MainActivity, "Map fragment", Toast.LENGTH_SHORT).show()
+                replaceFragment(MapFragment.newInstance())
+            }
+            ID_BUS -> {
+                Toast.makeText(this@MainActivity, "Bus fragment", Toast.LENGTH_SHORT).show()
+                replaceFragment(BusFragment.newInstance())
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(R.id.fragmentContainer, fragment).addToBackStack(Fragment::class.java.simpleName).commit()
+    }
+}
---
+++ autotrolej-app/app/src/main/java/com/example/autotrolejapp/models/LineResponse.kt
@@ -0,0 +1,6 @@
+package com.example.autotrolejapp.models
+
+
+// {"StanicaId":1734,"Naziv":"1. maja","Kratki":"1. maja","GpsX":14.43316,"GpsY":45.333713}
+class LineResponse {
+}
